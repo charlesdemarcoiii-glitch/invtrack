059e0d1f9ff09c6da24176343c499e041a743c9d
charlesdemarcoiii-glitch <charlesdemarcoiii@gmail.com>
2026-06-17 11:58:10 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE = 'invtrack-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  'https://cdn.jsdelivr.net/npm/jsqr@1.4.0/dist/jsQR.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('/index.html')))
+  );
+});
